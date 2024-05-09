fa239a94d8aeda9d26f79f0af44569af8f6bb5de
7dan2 <unic.norton@gmail.com>
2024-05-09 19:29:14 +0200

    add automatic authentication interface

--- JS/main.js
+++ JS/main.js
@@ -1,12 +1,26 @@
 function showAppName()
 {
-    const getDiv = document.getElementById("appLogo")
+    const getDiv = document.getElementById("appMultiArea")
     // getDiv.style.display = "block"
     getDiv.classList.add("showAppName")
+    
 }
 
 function coucou()
 {
-    setTimeout(showAppName, 3000)
+    setTimeout(showAppName, 2000)
+    document.getElementById("auth").classList.add("auth")
+    setTimeout(hideLogo, 5000)
+    setTimeout(showAuth, 5200)
     
+}
+
+function hideLogo()
+{
+    document.getElementById("appLogo").classList.add("hideLogo")
+}
+
+function showAuth()
+{
+    document.getElementById("auth").classList.remove("auth")
 }

b4213f242f8de108d8df44c0a2df1b0b91121bc6
7dan2 <unic.norton@gmail.com>
2024-06-11 20:52:06 +0200

    toggle filters

---
+++ JS/filters.js
@@ -0,0 +1,38 @@
+const getFiltersButton = document.getElementById("btn_searchFilters");
+getFiltersButton.addEventListener("click", myFunction)
+
+const getFilters = document.getElementById("filters");
+
+function toggleFiltersField()
+{
+    getFilters.classList.toggle("blabla")
+   
+}
+
+function myFunction() {
+    var x = document.getElementById("filters");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  } 
+
+const bdd = 
+[
+    {
+        site:"CMP",
+        batiment:0014,
+        equipements:
+        [
+            {
+                SSI:
+                [
+                    {
+                        BAESEvacuation:"R014023803001"
+                    }
+                ]
+            }
+        ]
+    }
+]
